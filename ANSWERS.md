195b637fe6b0c4e360f220d0da208a24f26caef8
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-08 12:02:44 +0100

    first commit

---
+++ ANSWERS.md
@@ -0,0 +1,12 @@
+# TP secure chat
+
+## Prise en main
+
+1. Cette topologie semble être une topologie en étoile.
+
+2. Dans les logs, on peut voir lorsque les deux clients se connectent au serveur. On peut également lire les messages, qui sont transmis en clair.
+
+3. C'est un problème, car cela viole le principe de confidentialité des données. 
+
+4. La solution la plus simple serait donc de mettre en place un chiffrement de données, de telle sorte à ce que seuls les interlocuteurs aient accès aux messages échangés.
+
---
+++ source/ciphered_gui.py
@@ -0,0 +1,54 @@
+import logging
+
+import dearpygui.dearpygui as dpg
+import cryptography
+
+from chat_client import ChatClient
+from generic_callback import GenericCallback
+from basic_gui import BasicGUI
+
+# default values used to populate connection window
+DEFAULT_VALUES = {
+    "host" : "127.0.0.1",
+    "port" : "6666",
+    "name" : "foo"
+}
+
+class CipheredGUI:
+    """
+    GUI for a chat secured client. Way more secured.
+    """
+    def __init__(self)->None:
+        # constructor
+        self._client = None
+        self._callback = None
+        self._log = logging.getLogger(self.__class__.__name__)
+
+
+    # surcharge pour inclure le champ de la clef de chiffrement
+    def _key(self)->None:
+        return 0
+
+    # surcharge de la fonction _create_connection_window() pour y inclure un champ password
+    def _create_connection_window():
+
+        with dpg.window(label="Connection", pos=(200, 150), width=400, height=300, show=False, tag="connection_windows"):
+            for field in ["host", "port", "name"]:
+                with dpg.group(horizontal=True):
+                    dpg.add_text(field)
+                    dpg.add_input_text(default_value=DEFAULT_VALUES[field], tag=f"connection_{field}")
+        
+        with dpg.window(label = "Password", pos = (400, 150)), width=400, height = 300, show = False, tag = "connection_windows"):
+            for field in ["host", "port", "name"]:
+                with dpg.group(horizontal = True):
+                    dpg.add_text(field)
+                    dpg.add_input_text(default_value=DEFAULT_VALUES[field], tag=f"connection_{field}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    # instanciate the class, create context and related stuff, run the main loop
+    client = CipheredGUI()
+    client.create()
+    client.loop()
