52af382bf5a9d4d08c4342b6ee96d12a2f33ad48
Darrys <darrys.abdelkrim@etu.univ-orleans.fr>
2023-03-25 22:40:29 +0100

    création du fichier time_fernet_gui

---
+++ source/time_fernet_gui.py
@@ -0,0 +1,98 @@
+from ciphered_gui import *
+from fernet_gui import *
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes, hmac
+import base64
+import hashlib
+import os
+
+
+TAILLE_CLEF_BLOC = 32 #taille de la clé en octets
+SEL = b"Gloire au Saint-Transistor" # création d'un sel fixe
+NB_ITERATIONS = 480000
+
+
+class TimeFernetGUI(FernetGUI):
+    """
+    GUI for a fernet chat secured client. Way way more secured.
+    """
+    def __init__(self)->None:
+        # constructor
+        self._client = None
+        self._callback = None
+        self._log = logging.getLogger(self.__class__.__name__)
+        # ajout du champ pour la clé de chiffrement
+        self._key = None
+
+
+    def run_chat(self, sender, app_data)->None:
+        self._log.info("Chat running...")
+        # callback used by the connection windows to start a chat session
+        host = dpg.get_value("connection_host")
+        port = int(dpg.get_value("connection_port"))
+        name = dpg.get_value("connection_name")
+
+        # on récupère le mot de passe
+        password = dpg.get_value("connection_password")
+
+        self._log.info(f"Connecting {name}@{host}:{port}")
+
+        self._callback = GenericCallback()
+
+        self._client = ChatClient(host, port)
+        self._client.start(self._callback)
+        self._client.register(name)
+
+        # génération de la clé à l'aide de SHA256 à partir du mot de passe
+        self._key = hashlib.sha256()
+        self._key.update(password.encode("utf-8"))
+        #self._key.digest()
+
+        self._log.info(f"Affichage de la clé : {self._key}")
+
+        # on convertit la clé en base 64
+        self._key = base64.b64encode(self._key.digest())
+
+        self._log.info(f"Affichage de la clé en base64 : {self._key}")
+
+        dpg.hide_item("connection_windows")
+        dpg.show_item("chat_windows")
+        dpg.set_value("screen", "Connecting")
+
+
+    # cette fonction sert à chiffrer les messages
+    def encrypt(self, message):
+        self._log.info("Début de chiffrement du message...")
+        
+        #chiffrement du message
+        self._log.info("Chiffrement du message...")
+        f = Fernet(self._key)
+        encrypted_message = f.encrypt(bytes(message, "utf-8"))
+        self._log.info("Message chiffré !")
+
+        self._log.info(f"[temporaire] Affichage du message chiffré : {encrypted_message}")
+
+        return(encrypted_message)
+
+
+    def decrypt(self, encrypted_message):
+        self._log.info("Déchiffrement du message...")
+
+        #conversion du vecteur d'initialisation et du message :
+        #iv = base64.b64decode(data[0]["data"])
+        encrypted_message = base64.b64decode(encrypted_message['data'])        
+
+        #déchiffrement du message
+        f = Fernet(self._key)
+        message = f.decrypt(encrypted_message)
+        message = str(message, "utf-8")
+        return(message)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    
+    # instanciate the class, create context and related stuff, run the main loop
+    client = FernetGUI()
+    client.create()
+    client.loop()
