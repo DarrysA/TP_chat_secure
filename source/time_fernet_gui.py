28dd2c6cb52cc3003ebfc55fcd3311a661cd7103
Darrys <darrys.abdelkrim@etu.univ-orleans.fr>
2023-03-26 10:52:19 +0200

    ajout de encrypt_at_time et decrypt_at_time

--- source/time_fernet_gui.py
+++ source/time_fernet_gui.py
@@ -16,49 +16,14 @@ class TimeFernetGUI(FernetGUI):
     """
     GUI for a fernet chat secured client. Way way more secured.
     """
-    def __init__(self)->None:
-        # constructor
-        self._client = None
-        self._callback = None
-        self._log = logging.getLogger(self.__class__.__name__)
-        # ajout du champ pour la clé de chiffrement
-        self._key = None
+    
+    def encrypt_at_time(self, message):
+        
+        return 
 
+    def decrypt_at_time(self, message):
 
-    def run_chat(self, sender, app_data)->None:
-        self._log.info("Chat running...")
-        # callback used by the connection windows to start a chat session
-        host = dpg.get_value("connection_host")
-        port = int(dpg.get_value("connection_port"))
-        name = dpg.get_value("connection_name")
-
-        # on récupère le mot de passe
-        password = dpg.get_value("connection_password")
-
-        self._log.info(f"Connecting {name}@{host}:{port}")
-
-        self._callback = GenericCallback()
-
-        self._client = ChatClient(host, port)
-        self._client.start(self._callback)
-        self._client.register(name)
-
-        # génération de la clé à l'aide de SHA256 à partir du mot de passe
-        self._key = hashlib.sha256()
-        self._key.update(password.encode("utf-8"))
-        #self._key.digest()
-
-        self._log.info(f"Affichage de la clé : {self._key}")
-
-        # on convertit la clé en base 64
-        self._key = base64.b64encode(self._key.digest())
-
-        self._log.info(f"Affichage de la clé en base64 : {self._key}")
-
-        dpg.hide_item("connection_windows")
-        dpg.show_item("chat_windows")
-        dpg.set_value("screen", "Connecting")
-
+        return
 
     # cette fonction sert à chiffrer les messages
     def encrypt(self, message):
